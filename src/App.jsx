7ff1f627f0a69e57dcba51b8d6ee1561400d61f8
Pawan Mahajan <pwnmhjn22@gmail.com>
2024-07-09 12:09:52 +0530

    try2

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
-
-import {createBrowserRouter , RouterProvider,Route,Link} from 'react-router-dom'
-import { Root } from './Pages'
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import { Root } from "./Pages";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
   },
   {
-    path: '/about',
+    path: "/about",
     element: <h2>About</h2>,
   },
   {
-    path: '/contact',
+    path: "/contact",
     element: <h2>Contact</h2>,
   },
   {
-    path: '*',
+    path: "*",
     element: <h2>Not Found</h2>,
   },
-])
+]);
 
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+//....dgwea
